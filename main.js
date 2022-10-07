87ba73f3c38188de6d8db988ecf0db4a03e67d5f
gloria-eme <glorymecas@hotmail.com>
2022-10-07 18:14:16 +0200

    amazing

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+`
+
+setupCounter(document.querySelector('#counter'))
+
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
